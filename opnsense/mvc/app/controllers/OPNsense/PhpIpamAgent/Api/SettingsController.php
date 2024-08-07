c1a5c4de1fae06f650deb6c577e824ffa34a7808
TotalGriffLock <john@theshado.ws>
2024-08-07 18:18:23 +0100

    Update SettingsController.php

--- opnsense/mvc/app/controllers/OPNsense/PhpIpamAgent/Api/SettingsController.php
+++ opnsense/mvc/app/controllers/OPNsense/PhpIpamAgent/Api/SettingsController.php
@@ -5,7 +5,7 @@ use \OPNsense\Base\ApiMutableModelControllerBase;
 use \OPNsense\PhpIpamAgent\PhpIpamAgent;
 use OPNsense\Core\Config;
 
-class SimplifiedSettingsController extends ApiMutableModelControllerBase
+class SettingsController extends ApiMutableModelControllerBase
 {
     protected static $internalModelName = 'phpipamagent';
     protected static $internalModelClass = 'OPNsense\PhpIpamAgent\PhpIpamAgent';
