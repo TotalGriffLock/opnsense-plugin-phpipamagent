a81d7e9bc47c14394f7a5174fa4be1574a00771f
TotalGriffLock <john@theshado.ws>
2024-08-07 15:05:21 +0100

    Create SimplifiedSettingsController.php
    Move to simple settings controller

---
+++ opnsense/mvc/app/controllers/OPNsense/PhpIpamAgent/Api/SimplifiedSettingsController.php
@@ -0,0 +1,5 @@
+class SimplifiedSettingsController extends ApiMutableModelControllerBase
+{
+    protected static $internalModelName = 'phpipamagent';
+    protected static $internalModelClass = 'OPNsense\PhpIpamAgent\PhpIpamAgent';
+}
