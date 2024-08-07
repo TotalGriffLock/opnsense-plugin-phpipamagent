41d31344224e1458de0601de208fd55abb6a5a60
TotalGriffLock <john@theshado.ws>
2024-08-07 18:37:57 +0100

    Update ServiceController.php
    Add reloadAction() function

--- opnsense/mvc/app/controllers/OPNsense/PhpIpamAgent/Api/ServiceController.php
+++ opnsense/mvc/app/controllers/OPNsense/PhpIpamAgent/Api/ServiceController.php
@@ -2,6 +2,20 @@
 namespace OPNsense\PhpIpamAgent\Api;
 
 use \OPNsense\Base\ApiControllerBase;
+use \OPNsense\Core\Backend;
+
 class ServiceController extends ApiControllerBase
 {
+  public function reloadAction()
+{
+    $status = "failed";
+    if ($this->request->isPost()) {
+        $backend = new Backend();
+        $bckresult = trim($backend->configdRun("template reload OPNsense/PhpIpamAgent"));
+        if ($bckresult == "OK") {
+            $status = "ok";
+        }
+    }
+    return array("status" => $status);
+}
 }
